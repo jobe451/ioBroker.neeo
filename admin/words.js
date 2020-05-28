392371e352d972c1fa8a4059e31ad045b1c5331e
Daniel Keller <jobe451@gmail.com>
2020-05-28 08:58:25 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"neeo adapter settings": {
+		"en": "Adapter settings for neeo",
+		"de": "Adaptereinstellungen für neeo",
+		"ru": "Настройки адаптера для neeo",
+		"pt": "Configurações do adaptador para neeo",
+		"nl": "Adapterinstellingen voor neeo",
+		"fr": "Paramètres d'adaptateur pour neeo",
+		"it": "Impostazioni dell'adattatore per neeo",
+		"es": "Ajustes del adaptador para neeo",
+		"pl": "Ustawienia adaptera dla neeo",
+		"zh-cn": "neeo的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
